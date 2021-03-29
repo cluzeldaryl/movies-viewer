c81e8d6e5fa3106c32bb8329752a96f29e0cf1a5
daryl <daryl.cluzel@yahoo.fr>
2021-03-29 13:09:06 +0200

    Initial commit

---
+++ src/app/shell/shell.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [CommonModule],
+  declarations: [ShellComponent],
+})
+export class ShellModule {}
